0fbeec1bfa80dbeec2218ed2741eeb87b297f8af
Oleh <mudrijoleg9@gmail.com>
2025-10-03 18:28:03 +0300

    Add full player UI and duration font modifier

--- MusicPlayer/View/PlayerView.swift
+++ MusicPlayer/View/PlayerView.swift
@@ -10,20 +10,113 @@ import SwiftUI
 struct PlayerView: View {
     
     @StateObject var viewModel = ViewModel()
-    @State var showFiles = false
+    @State private var showFiles = false
+    @State private var showFullPlayer = false
+    @Namespace private var playerAnimation
+    
+    var frameImage: CGFloat {
+        showFullPlayer ? 350 : 50
+    }
     
     var body: some View {
         NavigationStack {
             ZStack {
                 BackgroundView()
-                
-                List(viewModel.songs){ song in
-                    SongCell(song: song, durationFormatter: viewModel.durationFormatted)
-                        .onTapGesture {
-                            viewModel.playAudio(song: song)
+                VStack {
+                    
+                    // MARK: - List Of Song
+                    List(viewModel.songs){ song in
+                        SongCell(song: song, durationFormatter: viewModel.durationFormatted)
+                            .onTapGesture {
+                                viewModel.playAudio(song: song)
+                            }
+                    }
+                    .listStyle(.plain)
+                    
+                    Spacer()
+                    
+                    // MARK: - Player
+                    VStack {
+                        /// MARK: - MiniPlayer
+                        HStack {
+                            Color.white
+                                .frame(width: frameImage, height: frameImage)
+                            
+                            if !showFullPlayer {
+                                VStack(alignment: .leading) {
+                                    Text("Name")
+                                        .nameFont()
+                                    Text("Unknown Artist")
+                                        .artistFont()
+                                }
+                                .matchedGeometryEffect(id: "Song Title", in: playerAnimation)
+                                
+                                Spacer()
+                                
+                                CustomButton(image: "play.fill", size: .title) {
+                                    //action
+                                }
+                            }
+                            
+                            
                         }
+                        .padding()
+                        .background(showFullPlayer ? .clear : .black.opacity(0.3))
+                        .cornerRadius(10)
+                        .padding()
+                        
+                        /// MARK: - FullPlayer
+                        
+                        if showFullPlayer {
+                            VStack {
+                                VStack(alignment: .leading) {
+                                    Text("Name")
+                                        .nameFont()
+                                    Text("Unknown Artist")
+                                        .artistFont()
+                                }
+                            }.matchedGeometryEffect(id: "Song Title", in: playerAnimation)
+                                .padding(.top)
+                            
+                            VStack {
+                                HStack {
+                                    Text("00:00")
+                                        .artistFont()
+                                    Spacer()
+                                    Text("03:52")
+                                        .artistFont()
+                                }
+                                .durationFont()
+                                .padding()
+                                   
+                                Divider()
+                                
+                                HStack(spacing: 40) {
+                                    
+                                    CustomButton(image: "backward.end.fill", size: .title) {
+                                        // action
+                                    }
+                                    
+                                    CustomButton(image: "play.circle.fill", size: .largeTitle) {
+                                        // aciton
+                                    }
+                                    
+                                    
+                                    CustomButton(image: "forward.end.fill", size: .title) {
+                                        // action
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 40)
+                        }
+                    }
+                    .frame(height: showFullPlayer ? UIScreen.main.bounds.height + SizeConstant.fullPlayer : SizeConstant.miniPlayer)
+                    .onTapGesture {
+                        withAnimation(.spring) {
+                            self.showFullPlayer.toggle()
+                        }
+                    }
                 }
-                .listStyle(.plain)
             }
             
             // MARK: - Navigation Bar
@@ -40,6 +133,16 @@ struct PlayerView: View {
             .sheet(isPresented: $showFiles) {
                 ImportFileServise(songs: $viewModel.songs)
             }
+        }
+    }
+    
+    private func CustomButton(image: String, size: Font, action: @escaping () -> () ) -> some View {
+        Button {
+            action()
+        }label: {
+            Image(systemName: image)
+                .foregroundColor(.white)
+                .font(size)
         }
     }
 }
